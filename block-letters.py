2f37133a7ed18827d6f12e28708f96fe4fb7adf2
aishasmith <120410472+aishasmith@users.noreply.github.com>
2023-06-30 21:50:47 +0100

    first commit

---
+++ block-letters.py
@@ -0,0 +1,10 @@
+# Aisha Smith
+
+
+print("    A           SSSS        ")
+print("  A    A       S     S      ")
+print(" A      A      S            ")
+print(" AAAAAAAA       SSSSS       ")
+print(" A       A            S     ")
+print(" A       A     S      S     ")
+print(" A       A      SSSSSS      ")
